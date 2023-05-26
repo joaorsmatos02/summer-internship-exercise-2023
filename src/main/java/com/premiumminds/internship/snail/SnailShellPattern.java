7e7f782529a907eb6588ebea4352d6c47176fadd
João Matos <fc56292@alunos.fc.ul.pt>
2023-05-26 20:33:33 +0100

    initial commit

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,5 +1,6 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 /**
@@ -7,13 +8,24 @@ import java.util.concurrent.Future;
  */
 class SnailShellPattern implements ISnailShellPattern {
 
-  /**
-   * Method to get snailshell pattern
-   * 
-   * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
-   */
-  public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+	/**
+	 * Method to get snailshell pattern
+	 * 
+	 * @param matrix matrix of numbers to go through
+	 * @return order array of values thar represent a snail shell pattern
+	 */
+	public Future<int[]> getSnailShell(int[][] matrix) {
+		CompletableFuture<int[]> future = new CompletableFuture<>();
+
+		int[] result = new int[matrix.length * matrix[0].length];
+		while (matrix.length != 0) {
+			// add the first array in the matrix to the result
+			// get the last element from each array and add to result
+			// reverse the last array in the matrix and add to result
+			// get the first element from each array, reverse and add to result
+		}
+
+		future.complete(result);
+		return future;
+	};
 }
