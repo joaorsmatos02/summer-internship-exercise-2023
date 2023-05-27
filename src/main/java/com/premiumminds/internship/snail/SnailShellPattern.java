5d4c8a413992051b9747405b756e38caaa8617d7
João Matos <fc56292@alunos.fc.ul.pt>
2023-05-27 22:02:15 +0100

    Optimization, bug fixes and added tests

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -5,87 +5,62 @@ import java.util.concurrent.Future;
 
 class SnailShellPattern implements ISnailShellPattern {
 
+	@Override
 	public Future<int[]> getSnailShell(int[][] matrix) {
 		CompletableFuture<int[]> future = new CompletableFuture<>();
 
-		// initial checks
-		if (matrix.length == 0) { // if it is empty
-			future.complete(new int[0]);
-			return future;
-		} else if (matrix.length == 1) { // if it is only horizontal
-			future.complete(matrix[0]);
-			return future;
-		} else if (matrix[0].length == 1) { // if it is only vertical
-			// TODO
-			return future;
-		}
-
-		int resultItems = 0;
+		// Create a result array and an index pointer
 		int[] result = new int[matrix.length * matrix[0].length];
-		for (int i = 0; i < countIterations(matrix); i++) {
-			resultItems = getTop(matrix, i, result, resultItems);
-			resultItems = getRight(matrix, i, result, resultItems);
-			resultItems = getBottom(matrix, i, result, resultItems);
-			resultItems = getLeft(matrix, i, result, resultItems);
+		int resultIndex = 0;
+
+		// Count the number of iterations (completed laps around the matrix)
+		int totalIterations = Math.min(matrix.length / 2, matrix[0].length / 2);
+		for (int iterationCount = 0; iterationCount < totalIterations; iterationCount++) {
+
+			// Traverse top row
+			for (int i = iterationCount; i < matrix[iterationCount].length - iterationCount; i++) {
+				result[resultIndex] = matrix[iterationCount][i];
+				resultIndex++;
+			}
+
+			// Traverse right column
+			for (int i = iterationCount + 1; i < matrix.length - iterationCount - 1; i++) {
+				result[resultIndex] = matrix[i][matrix[i].length - 1 - iterationCount];
+				resultIndex++;
+			}
+
+			// Traverse bottom row
+			for (int i = matrix[iterationCount].length - iterationCount - 1; i >= iterationCount; i--) {
+				result[resultIndex] = matrix[matrix.length - 1 - iterationCount][i];
+				resultIndex++;
+			}
+
+			// Traverse left column
+			for (int i = matrix.length - 2 - iterationCount; i > iterationCount; i--) {
+				result[resultIndex] = matrix[i][iterationCount];
+				resultIndex++;
+			}
 		}
 
-		// if the matrix has odd dimensions, there will be an incomplete iteration left
-		// at the end
-		getRemainder(matrix, result, resultItems);
+		// Handle incomplete iterations
+		int remainingRows = matrix.length - 2 * totalIterations;
+		int remainingCols = matrix[0].length - 2 * totalIterations;
+		if (remainingRows == 1) {
+			int row = matrix.length / 2;
+			for (int i = totalIterations; resultIndex != result.length; i++) {
+				result[resultIndex] = matrix[row][i];
+				resultIndex++;
+			}
+		} else if (remainingCols == 1) {
+			int col = matrix[0].length / 2;
+			for (int i = totalIterations; resultIndex != result.length; i++) {
+				result[resultIndex] = matrix[i][col];
+				resultIndex++;
+			}
+		}
 
 		future.complete(result);
 		return future;
 	}
 
-	private int countIterations(int[][] matrix) {
-		return Math.min(matrix.length / 2, matrix[0].length / 2);
-	}
-
-	private int getTop(int[][] matrix, int iteration, int[] result, int resultItems) {
-		for (int i = iteration; i < matrix[iteration].length - iteration; i++) {
-			result[resultItems] = matrix[iteration][i];
-			resultItems++;
-		}
-		return resultItems;
-	}
-
-	private int getRight(int[][] matrix, int iteration, int[] result, int resultItems) {
-		for (int i = iteration + 1; i < matrix.length - iteration - 1; i++) {
-			result[resultItems] = matrix[i][matrix[i].length - 1 - iteration];
-			resultItems++;
-		}
-		return resultItems;
-	}
-
-	private int getBottom(int[][] matrix, int iteration, int[] result, int resultItems) {
-		for (int i = matrix[iteration].length - iteration - 1; i >= iteration; i--) {
-			result[resultItems] = matrix[matrix.length - 1 - iteration][i];
-			resultItems++;
-		}
-		return resultItems;
-	}
-
-	private int getLeft(int[][] matrix, int iteration, int[] result, int resultItems) {
-		for (int i = matrix.length - 2 - iteration; i > iteration; i--) {
-			result[resultItems] = matrix[i][iteration];
-			resultItems++;
-		}
-		return resultItems;
-	}
-
-	private void getRemainder(int[][] matrix, int[] result, int resultItems) {
-		if (matrix.length % 2 == 1) {
-			int row = matrix.length / 2;
-			for (int i = countIterations(matrix); resultItems != result.length; i++) {
-				result[resultItems] = matrix[row][i];
-				resultItems++;
-			}
-		} else if (matrix[0].length % 2 == 1) {
-			int col = matrix[0].length / 2;
-			for (int i = countIterations(matrix); resultItems != result.length; i++) {
-				result[resultItems] = matrix[i][col];
-				resultItems++;
-			}
-		}
-	}
 }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -1,6 +1,8 @@
 package com.premiumminds.internship.snail;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -16,22 +18,58 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class SnailShellPatternTest {
 
-  /**
-   * The corresponding implementations to test.
-   *
-   * If you want, you can make others :)
-   *
-   */
-  public SnailShellPatternTest() {
-  };
+	/**
+	 * The corresponding implementations to test.
+	 *
+	 * If you want, you can make others :)
+	 *
+	 */
+	public SnailShellPatternTest() {
+	};
 
-  @Test
-  public void ScreenLockinPatternTestFirst3Length2Test()
-      throws InterruptedException, ExecutionException, TimeoutException {
-    int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
-    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
-    int[] result = count.get(10, TimeUnit.SECONDS);
-    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    assertEquals(result, expected);
-  }
+	@Test
+	public void ScreenLockinPatternTestFirst3Length2Test()
+			throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
+	public void TestEmpty() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { {} };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = {};
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
+	public void Test1by3() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1, 2, 3 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1, 2, 3 };
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
+	public void Test3by1() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1 }, { 2 }, { 3 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1, 2, 3 };
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
+	public void Test2by3() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1, 2, 3 }, { 6, 5, 4 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1, 2, 3, 4, 5, 6 };
+		assertTrue(Arrays.equals(result, expected));
+	}
 }
