74298186b79d1dc1b4983e421c9c2e23c495fc3d
João Matos <fc56292@alunos.fc.ul.pt>
2023-05-28 10:12:32 +0100

    Organized code and added documentation

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -19,43 +19,108 @@ class SnailShellPattern implements ISnailShellPattern {
 		int left = 0;
 		int right = matrix[0].length - 1;
 
-		while (top <= bottom && left <= right) {
+		// while result has less elements than matrix
+		while (resultIndex < result.length) {
 
 			// top boundary
-			for (int i = left; i <= right; i++) {
-				result[resultIndex] = matrix[top][i];
-				resultIndex++;
-			}
+			resultIndex = traverseTopBoundary(matrix, result, resultIndex, top, left, right);
 			top++;
 
 			// right boundary
-			for (int i = top; i <= bottom; i++) {
-				result[resultIndex] = matrix[i][right];
-				resultIndex++;
-			}
+			resultIndex = traverseRightBoundary(matrix, result, resultIndex, right, top, bottom);
 			right--;
 
 			// bottom boundary
 			if (top <= bottom) {
-				for (int i = right; i >= left; i--) {
-					result[resultIndex] = matrix[bottom][i];
-					resultIndex++;
-				}
+				resultIndex = traverseBottomBoundary(matrix, result, resultIndex, bottom, left, right);
 				bottom--;
 			}
 
 			// left boundary
 			if (left <= right) {
-				for (int i = bottom; i >= top; i--) {
-					result[resultIndex] = matrix[i][left];
-					resultIndex++;
-				}
+				resultIndex = traverseLeftBoundary(matrix, result, resultIndex, left, top, bottom);
 				left++;
 			}
 		}
 
 		future.complete(result);
 		return future;
+	}
+
+	/**
+	 * Traverses the top boundary of the matrix and adds the elements to the result
+	 * array.
+	 *
+	 * @param matrix      the input matrix
+	 * @param result      the result array
+	 * @param resultIndex the current index in the result array
+	 * @param top         the current top boundary index
+	 * @param left        the current left boundary index
+	 * @param right       the current right boundary index
+	 * @return the updated result index after traversing the top boundary
+	 */
+	private int traverseTopBoundary(int[][] matrix, int[] result, int resultIndex, int top, int left, int right) {
+		for (int i = left; i <= right; i++) {
+			result[resultIndex++] = matrix[top][i];
+		}
+		return resultIndex;
+	}
+
+	/**
+	 * Traverses the right boundary of the matrix and adds the elements to the
+	 * result array.
+	 *
+	 * @param matrix      the input matrix
+	 * @param result      the result array
+	 * @param resultIndex the current index in the result array
+	 * @param right       the current right boundary index
+	 * @param top         the current top boundary index
+	 * @param bottom      the current bottom boundary index
+	 * @return the updated result index after traversing the right boundary
+	 */
+	private int traverseRightBoundary(int[][] matrix, int[] result, int resultIndex, int right, int top, int bottom) {
+		for (int i = top; i <= bottom; i++) {
+			result[resultIndex++] = matrix[i][right];
+		}
+		return resultIndex;
+	}
+
+	/**
+	 * Traverses the bottom boundary of the matrix and adds the elements to the
+	 * result array.
+	 *
+	 * @param matrix      the input matrix
+	 * @param result      the result array
+	 * @param resultIndex the current index in the result array
+	 * @param bottom      the current bottom boundary index
+	 * @param left        the current left boundary index
+	 * @param right       the current right boundary index
+	 * @return the updated result index after traversing the bottom boundary
+	 */
+	private int traverseBottomBoundary(int[][] matrix, int[] result, int resultIndex, int bottom, int left, int right) {
+		for (int i = right; i >= left; i--) {
+			result[resultIndex++] = matrix[bottom][i];
+		}
+		return resultIndex;
+	}
+
+	/**
+	 * Traverses the left boundary of the matrix and adds the elements to the result
+	 * array.
+	 *
+	 * @param matrix      the input matrix
+	 * @param result      the result array
+	 * @param resultIndex the current index in the result array
+	 * @param left        the current left boundary index
+	 * @param top         the current top boundary index
+	 * @param bottom      the current bottom boundary index
+	 * @return the updated result index after traversing the left boundary
+	 */
+	private int traverseLeftBoundary(int[][] matrix, int[] result, int resultIndex, int left, int top, int bottom) {
+		for (int i = bottom; i >= top; i--) {
+			result[resultIndex++] = matrix[i][left];
+		}
+		return resultIndex;
 	}
 
 }
