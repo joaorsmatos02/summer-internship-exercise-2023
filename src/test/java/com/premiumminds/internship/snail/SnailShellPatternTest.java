ce5ee41359f9cca8c84bedc2acc58d42c1e88d26
João Matos <fc56292@alunos.fc.ul.pt>
2023-06-02 14:07:59 +0100

    Small fix on tests

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -47,6 +47,15 @@ public class SnailShellPatternTest {
 	}
 
 	@Test
+	public void Test1by1() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1 };
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
 	public void Test1by3() throws InterruptedException, ExecutionException, TimeoutException {
 		int[][] matrix = { { 1, 2, 3 } };
 		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
@@ -72,4 +81,37 @@ public class SnailShellPatternTest {
 		int[] expected = { 1, 2, 3, 4, 5, 6 };
 		assertTrue(Arrays.equals(result, expected));
 	}
+
+	@Test
+	public void Test3by4() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = { { 1, 2, 3, 4 }, { 10, 11, 12, 5 }, { 9, 8, 7, 6 } };
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+		assertTrue(Arrays.equals(result, expected));
+	}
+
+	@Test
+	public void TestLargeMatrix() throws InterruptedException, ExecutionException, TimeoutException {
+		int[][] matrix = new int[1000][1000];
+		for (int i = 0; i < 1000; i++) {
+			for (int j = 0; j < 1000; j++) {
+				if ((i + j) % 2 == 0)
+					matrix[i][j] = 1;
+				else
+					matrix[i][j] = 0;
+			}
+		}
+		Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+		int[] result = count.get(10, TimeUnit.SECONDS);
+		int[] expected = new int[1000000];
+		for (int i = 0; i < expected.length; i++) {
+			if (i % 2 == 0)
+				expected[i] = 1;
+			else
+				expected[i] = 0;
+		}
+		assertTrue(Arrays.equals(result, expected));
+	}
+
 }
